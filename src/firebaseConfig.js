11ff94a3ae5ba985647af78d2930299fbf8e0f8b
andiichwannn <andiichwann@gmail.com>
2025-01-30 14:18:31 +0800

    fix firebaseConfig

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,28 +1,23 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from 'firebase/firestore';  // Import Firestore
+import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyB5-x2X8IIbKCvn7OAsbUkDvFB52_jyR9A",
-    authDomain: "samudera-web-cbf2f.firebaseapp.com",
-    projectId: "samudera-web-cbf2f",
-    storageBucket: "samudera-web-cbf2f.firebasestorage.app",
-    messagingSenderId: "914292170028",
-    appId: "1:914292170028:web:c415b02f810d92bfd9e089",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Mendapatkan instance Auth dan Firestore
 const auth = getAuth(app);
-const db = getFirestore(app);  
+const db = getFirestore(app);
 const storage = getStorage(app)
 
-// Initialize Firebase Authentication and export it
 export default app;
 
 export { auth, db, storage };  
