4149d454de67936657fafb0aa172894ed3ea148a
nurulimamah <nurulimamahtamrin03@gmail.com>
2026-04-15 15:42:59 +0800

    penambahan unit bisnis pada edit users

--- src/components/FormEditUser.jsx
+++ src/components/FormEditUser.jsx
@@ -60,7 +60,8 @@ const EditUserForm = () => {
         { value: 'PT SILKargo Indonesia', label: 'PT SILKargo Indonesia' },
         { value: 'PT PAD Samudera Perdana', label: 'PT PAD Samudera Perdana' },
         { value: 'PT Masaji Kargosentra Tama', label: 'PT Masaji Kargosentra Tama' },
-        { value: 'Samudera', label: 'Samudera' }
+        { value: 'Samudera Indonesia', label: 'Samudera Indonesia' },
+        {value: 'Panitia', label: 'Panitia' }
     ]
 
     // Options department
